86164481cad7d9a6085d830d29f09e86ead5f12e
eslamadel <eslam.adel.mahmoud.ali@gmail.com>
2018-03-13 03:14:56 +0200

    Working so far

--- hough.py
+++ hough.py
@@ -8,30 +8,33 @@ def houghLine(image):
     thetas = np.deg2rad(np.arange(-90, 90))
     
     #Get image dimensions
-    width = image.shape[0]
-    hight = image.shape[1]
+    Ny = image.shape[0]
+    Nx = image.shape[1]
     
     #Max diatance is diagonal one 
-    Maxdist = int(np.round(np.sqrt(width**2 + hight ** 2)))
+    Maxdist = int(np.round(np.sqrt(Nx**2 + Ny ** 2)))
     
     #Range of radius
     rs = np.linspace(-Maxdist, Maxdist, 2*Maxdist)
     accumulator = np.zeros((2 * Maxdist, len(thetas)))
     
     
-    for i in range(width):
-        for j in range(hight):
-             if image[i,j] > 0:
+    for y in range(Ny):
+        for x in range(Nx):
+             if image[y,x] > 0:
                  for k in range(len(thetas)):
-                     r = i*np.cos(thetas[k]) + j * np.sin(thetas[k])
+                     r = x*np.cos(thetas[k]) + y * np.sin(thetas[k])
                      accumulator[int(r) + Maxdist,k] += 1
     return accumulator, thetas, rs
 
 
 def detectLines(image,accumulator, threshold, rohs, thetas):
+    Nx = image.shape[1]
+    Ny = image.shape[0]
     maxVal = np.max(accumulator)
     sortedAcc = np.argsort(accumulator, axis=None)
     lineIdxs = []
+    #Maxdist = int(np.round(np.sqrt(Nx**2 + Ny ** 2)))
     for i in reversed(sortedAcc):
         if accumulator[int(i/accumulator.shape[1]),int(i%accumulator.shape[1])] >= threshold*maxVal: 
             lineIdxs.append(i)
@@ -40,19 +43,20 @@ def detectLines(image,accumulator, threshold, rohs, thetas):
     selectedRos = []
     selectedThetas = []
     for idx in lineIdxs:
-        rho = rhos[int(idx / accumulator.shape[1])]
+        rho = rhos[int(idx / accumulator.shape[1])] 
         selectedRos.append(rho)
-        theta = thetas[int(idx % accumulator.shape[1])]
+        theta = thetas[int(idx % accumulator.shape[1])] 
         selectedThetas.append(theta)
         #plotLine(image, rho, theta)
     plotLine(image,selectedRos,selectedThetas)
         
 def plotLine(image, rhos, thetas):
     Nx = image.shape[1]
+    Ny = image.shape[0]
     x = range(Nx)
     fig, ax = plt.subplots()
-    #plt.xlim(0, image.shape[0])
-    #plt.ylim(0,image.shape[1])
+    plt.xlim(0,Nx)
+    plt.ylim(Ny,0)
     ax.imshow(image)
     for i in range(len(rhos)):
         rho = rhos[i]
@@ -63,9 +67,9 @@ def plotLine(image, rhos, thetas):
     
     
 if __name__ == '__main__':
-    image = plt.imread('images/Lines.jpg')
+    image = plt.imread('images/Regular-Shapes.jpg')
     channel = image[...,2]
-    channel = imresize(channel,(200,250))
+    #    
     edgeImage = feature.canny(channel)
 
     #image = np.zeros((50,50))
@@ -75,7 +79,7 @@ if __name__ == '__main__':
     
     accumulator, thetas, rhos = houghLine(edgeImage)
     plt.figure('Original Image')
-    plt.imshow(image)
+    plt.imshow(edgeImage)
     plt.set_cmap('gray')
     plt.figure('Hough Space')
     plt.imshow(accumulator)
