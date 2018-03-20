d50b24ed301714d9872df32892a46afc43206ac8
eslamadel <eslam.adel.mahmoud.ali@gmail.com>
2018-03-20 08:15:44 +0200

    minor

--- corner.py
+++ corner.py
@@ -73,9 +73,9 @@ def compute_harris_response(im,sigma=3):
     imy = np.zeros(im.shape)
     filters.gaussian_filter(im, (sigma,sigma), (1,0), imy)
     # compute components of the Harris matrix
-    Wxx = filters.gaussian_filter(imx*imx,sigma)
+    Wxx = filters.gaussian_filter(imx**2,sigma)
     Wxy = filters.gaussian_filter(imx*imy,sigma)
-    Wyy = filters.gaussian_filter(imy*imy,sigma)
+    Wyy = filters.gaussian_filter(imy**2,sigma)
     # determinant and trace
     Wdet = Wxx*Wyy - Wxy**2
     Wtr = Wxx + Wyy
