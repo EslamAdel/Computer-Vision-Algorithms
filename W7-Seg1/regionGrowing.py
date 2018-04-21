daf5e4173a000ef7ce7f6913b1beaa6e4829bdf7
eslamadel <eslam.adel.mahmoud.ali@gmail.com>
2018-04-21 09:36:42 +0200

    Working grwoing based on intensity TODO: Based on color

--- W7-Seg1/regionGrowing.py
+++ W7-Seg1/regionGrowing.py
@@ -3,66 +3,52 @@ import matplotlib.pyplot as plt
 from matplotlib import colors
 
 
-def idxFromPosition(pos, ImShape):
-    idx = pos[0]*ImShape[1] + pos[1]
-    return np.int(idx)
-
-def posFromIdx(idx, ImShape):
-    pos = np.zeros(2,np.int)   
-    pos[0] = np.int(idx/ImShape[1]) 
-    pos[1] = np.int(idx % ImShape[1])
-    return pos
-
 def onClick(event):
     x, y = int(event.xdata), int(event.ydata)
     outIm = regionGrowing(valChannel,np.array([y,x]),10)
+    segmentedImage = np.copy(myImage)
+    segmentedImage[...,0] *= outIm.astype('uint8')
+    segmentedImage[...,1] *= outIm.astype('uint8')
+    segmentedImage[...,2] *= outIm.astype('uint8')
     plt.figure()
-    plt.imshow(outIm)    
+    plt.imshow(segmentedImage)    
     plt.show()
-
-def onClose(event):
-    exit(0)
     
 def regionGrowing(image, seed, threshold):
-    points = np.zeros(image.shape[0]*image.shape[1])
-    VisitedPoints = np.zeros(image.shape[0]*image.shape[1])
+    # Initilize points List
+    points = []
     outImage = np.zeros(image.shape)
-    points[idxFromPosition(seed,image.shape)] = 1
-    VisitedPoints[idxFromPosition(seed,image.shape)] = 1 
+    points.append(seed)
     outImage[seed[0],seed[1]] = 1
-    while np.size(np.where(points == 1)):
-        allPts = np.array(np.where(points == 1)).T
-        p = posFromIdx(allPts[0],image.shape)
-        points[allPts[0]] = 0
+    while len(points):
+        p = points.pop()
         neighbors = np.array([[p[0]-1,p[1]] ,
                               [p[0]+1,p[1]] ,
                               [p[0], p[1]-1],
                               [p[0], p[1]+1]])
         try:
             for i in range(4):
-                if not VisitedPoints[idxFromPosition(neighbors[i,:], image.shape)]:
+                if not outImage[neighbors[i,0],neighbors[i,1]]:
                     if np.abs(image[neighbors[i,0],neighbors[i,1]] - image[p[0],p[1]]) <= threshold:
                         outImage[neighbors[i,0],neighbors[i,1]] = 1
-                        points[idxFromPosition(neighbors[i,:], image.shape)] = 1
-                        VisitedPoints[idxFromPosition(neighbors[i,:], image.shape)] = 1
+                        points.append(np.array([neighbors[i,0],neighbors[i,1]]))
         except:
             continue
-                
-                
+        
     return outImage
                 
 
 
 if __name__ == '__main__':
-    image = plt.imread('images/seg1.jpg')
-    hsvImage = colors.rgb_to_hsv(image)
+    global myImage
+    myImage = plt.imread('images/seg1.jpg')
+    hsvImage = colors.rgb_to_hsv(myImage)
     global valChannel 
     valChannel = hsvImage[...,2]
     fig = plt.figure()
-    plt.imshow(valChannel)
+    plt.imshow(myImage)
     plt.set_cmap('gray')
     fig.canvas.mpl_connect('button_press_event', onClick)
-    fig.canvas.mpl_connect('close_event', onClose)
     plt.show()
     
     
